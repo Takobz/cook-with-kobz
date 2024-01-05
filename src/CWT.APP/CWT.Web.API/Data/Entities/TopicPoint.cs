1e89d2adac484faa7f4504f5c2af80a98cc6f284
takobz <kobelakhutso.kk@gmail.com>
2024-01-05 06:41:07 +0200

    morning playing around

--- src/CWT.APP/CWT.Web.API/Data/Entities/TopicPoint.cs
+++ src/CWT.APP/CWT.Web.API/Data/Entities/TopicPoint.cs
@@ -2,8 +2,8 @@ namespace CWT.WEB.API.Data.Entities
 {
     public class TopicPoint : Entity 
     {
-        public string TopicLine { get; set; } = string.Empty;
-        public List<Invitee> Contributors { get; set; } = new List<Invitee>();
+        public string TopicLine { get; private set; } = string.Empty;
+        public List<Invitee> Contributors { get; private set; } = new List<Invitee>();
         //how do we see my contributions?
     }
 }
--- src/CWT.APP/CWT.Web/cwt-web-app/src/components/landingpage/Login.tsx
+++ src/CWT.APP/CWT.Web/cwt-web-app/src/components/landingpage/Login.tsx
@@ -1,23 +1,39 @@
 import { Button, Card, CardContent, CardHeader, Divider, Link, TextField } from "@mui/material"
 import '../../App.css'
+import { useState } from "react";
 
 //loading logic:
 //disable textfields and buttons, causing login button to show loading
 
-const Login = () => {
+const Login = () => {   
+    const [isLoginLoading, setLoginLoading] = useState(false);
+    const handleLogin = () => {
+        setLoginLoading(true);
+
+        //use fetch here.
+        //recommended to use fetch instead of useEffect
+        //because useEffect is for side effects, not for data fetching (even though it can be used for data fetching)
+        //useEffect is better when we want to do something when a component is mounted or unmounted.
+    }
+
     return (
         <div className="CenterDiv" style={{ background: 'gray', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }}>
             <Card sx={{ maxWidth: 500, margin: '10px', width: '75%' }} >
                 <CardHeader 
-                    title='Start Cooking...'
+                    title='Start Cooking'
                     sx={{ fontWeight: 'bold' }}
-                    titleTypographyProps={{ variant:'h3' }}/>
+                    titleTypographyProps={{ variant:'h4' }}/>
                 <CardContent>
                     <TextField id='username' label='Username' variant='outlined' fullWidth sx={{ mb: 2 }}/>
                     <TextField id='password' label='Password' variant='outlined' type='password' fullWidth sx={{ mb: 2 }}/>
-                    <Button variant="contained" disableElevation sx={{ width: '100%' }}>
+                    {isLoginLoading ? 
+                    <Button variant="contained" disableElevation sx={{ width: '100%' }} disabled>
+                        <b>Login</b>
+                    </Button> :
+                    <Button variant="contained" disableElevation sx={{ width: '100%' }} onClick={handleLogin}>
                         <b>Login</b>
                     </Button>
+                    }
                     <div className="CenterDiv" style={{ textAlign: 'center', margin: '10px'}}>
                         <Link href="#">Forgot Password? Reset</Link>
                     </div>
